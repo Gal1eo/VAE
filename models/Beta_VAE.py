fix KL

models/Beta_VAE.py
@@ -235,7 +235,7 @@ class BetaVAE(BaseVAE):
         :param kwargs:
         :return: The KL divergence of D(posterior, prior)
         """
-        loss_1 = -0.5 * torch.sum((prior.covariance_matrix.sum(1)).log() + (posterior.covariance_matrix.sum(1)).log())
+        loss_1 = -0.5 * torch.sum((prior.covariance_matrix.sum(1)) + (posterior.covariance_matrix.sum(1)).log())
         loss_2 = -0.5 * torch.sum(torch.sum(posterior.mean ** 2) + posterior.covariance_matrix)
         loss = loss_1 - loss_2
 